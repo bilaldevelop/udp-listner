db95e832b5915bfab91927d958611edd0d7ab1c2
Bilal ABDULHAMID <babdulhamid@kalyonpv.com>
2022-02-16 10:15:25 +0300

    version 1.0

---
+++ UDPClient/UDPClient/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+            string input, stringData;
+
+            //create TCP server
+            ClientText();
+            Console.WriteLine("This is a Client, host name is {0}", Dns.GetHostName());
+
+            //setup setver IP，set up TCP port
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8001);
+
+            //set up protocol type ，data connection type and data type UDP
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            string welcome = "Hello! ";
+            data = Encoding.ASCII.GetBytes(welcome);
+            server.SendTo(data, data.Length, SocketFlags.None, ipep);
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)sender;
+
+            data = new byte[1024];
+            int recv = server.ReceiveFrom(data, ref Remote);
+            Console.WriteLine("Message received from {0}: ", Remote.ToString());
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "exit")
+                    break;
+                server.SendTo(Encoding.ASCII.GetBytes(input), Remote);
+                data = new byte[1024];
+                recv = server.ReceiveFrom(data, ref Remote);
+                stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine(stringData);
+            }
+            Console.WriteLine("Stopping Client.");
+            server.Close();
+        }
+
+
+        private static void ClientText()
+        {
+            string text = $@"
+
+  _    _ _____  _____     _____ _ _            _   
+ | |  | |  __ \|  __ \   / ____| (_)          | |  
+ | |  | | |  | | |__) | | |    | |_  ___ _ __ | |_ 
+ | |  | | |  | |  ___/  | |    | | |/ _ \ '_ \| __|
+ | |__| | |__| | |      | |____| | |  __/ | | | |_ 
+  \____/|_____/|_|       \_____|_|_|\___|_| |_|\__|
+                                                   
+                                                   
+                                                                                                         
+";
+
+            Console.WriteLine(text);
+        }
+
+    }
+}
+
---
+++ UDPServer/UDPServer/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+
+            int recv;
+            byte[] data = new byte[1024];
+
+            //create TCP server
+            ServerText();
+            //get local IP，set TCP port         
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 8001);
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            //binding network address
+            newsock.Bind(ipep);
+
+            Console.WriteLine("This is a Server, host name is {0}", Dns.GetHostName());
+
+            //wait client connectiong
+            Console.WriteLine("Waiting for a client...");
+
+            //get client IP
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)(sender);
+            recv = newsock.ReceiveFrom(data, ref Remote);
+            Console.WriteLine("Message received from {0}: ", Remote.ToString());
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+
+            //client connected successed after send welcome message
+            string welcome = "Welcome ! ";
+
+            // string convert to byte array data
+            data = Encoding.ASCII.GetBytes(welcome);
+
+            //send message
+            newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+            while (true)
+            {
+                data = new byte[1024];
+                //send accept message
+                recv = newsock.ReceiveFrom(data, ref Remote);
+                Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+                newsock.SendTo(data, recv, SocketFlags.None, Remote);
+            }
+        }
+
+
+        private static void ServerText()
+        {
+            string text = $@"
+
+ _   _____________   _____                          
+| | | |  _  \ ___ \ /  ___|                         
+| | | | | | | |_/ / \ `--.  ___ _ ____   _____ _ __ 
+| | | | | | |  __/   `--. \/ _ \ '__\ \ / / _ \ '__|
+| |_| | |/ /| |     /\__/ /  __/ |   \ V /  __/ |   
+ \___/|___/ \_|     \____/ \___|_|    \_/ \___|_|   
+                                                    
+                                                                                                                                                         
+";
+
+            Console.WriteLine(text);
+        }
+
+    }
+}
